c51e62bf6a2c614a2f87bcea0c1181244579cb07
G8Mannn <1726836740@qq.com>
2026-04-25 22:43:27 +0800

    no message

--- grey.h
+++ grey.h
@@ -5,9 +5,13 @@
 
 #define CHANNELS 8
 
-#define AD0_WRITE(state) HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
-#define AD1_WRITE(state) HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
-#define AD2_WRITE(state) HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
+#define AD0_PIN GPIO_PIN_4
+#define AD1_PIN GPIO_PIN_5
+#define AD2_PIN GPIO_PIN_6
+
+#define AD0_WRITE(state) HAL_GPIO_WritePin(GPIOA,AD0_PIN,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
+#define AD1_WRITE(state) HAL_GPIO_WritePin(GPIOA,AD1_PIN,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
+#define AD2_WRITE(state) HAL_GPIO_WritePin(GPIOA,AD2_PIN,(state)?GPIO_PIN_SET:GPIO_PIN_RESET)
 
 static void select_channel(uint8_t channel);
 static uint16_t grey_outread(void);
